5a350d4381d270a2f910d97e38bc7cbc5b63db2b
Butch Mayhew <bmayhew@tilled.com>
2023-07-17 03:01:43 -0500

    adding examples using storageState (#1)

--- lib/pages/loginPage.ts
+++ lib/pages/loginPage.ts
@@ -1,12 +1,23 @@
 import { Page } from "@playwright/test";
 
 export class LoginPage {
-  readonly username = this.page.locator('[data-test="email"]');
-  readonly password = this.page.locator('[data-test="password"]');
-  readonly submit = this.page.locator('[data-test="login-submit"]');
+  readonly username = this.page.getByTestId("email");
+  readonly password = this.page.getByTestId("password");
+  readonly submit = this.page.getByTestId("login-submit");
+  readonly navUserMenu = this.page.getByTestId("nav-user-menu");
+  readonly navAdminMenu = this.page.getByTestId("nav-admin-menu");
+  readonly navSignOut = this.page.getByTestId("nav-sign-out");
+  readonly navSignIn = this.page.getByTestId("nav-sign-in");
 
   async goto() {
     await this.page.goto("/#/auth/login");
+  }
+
+  async login(email: string, password: string) {
+    await this.goto();
+    await this.username.fill(email);
+    await this.password.fill(password);
+    await this.submit.click();
   }
 
   constructor(private readonly page: Page) {}
