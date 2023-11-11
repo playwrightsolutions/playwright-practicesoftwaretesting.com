14141f2c9e6eca5bdc6b07767b793fdfc37cb052
Butch Mayhew <bmayhew@tilled.com>
2023-11-11 16:47:30 -0600

    updating to add currents.dev reporter in CI

--- tests/checkout/checkoutWithRoute.spec.ts
+++ tests/checkout/checkoutWithRoute.spec.ts
@@ -2,22 +2,18 @@ import { expect } from "@playwright/test";
 import { test, HomePage } from "@pages";
 
 test.describe("UI with API Routing", () => {
+  const productApiUrl = "https://api.practicesoftwaretesting.com/products?**";
   test("Checking for disposed api context", async ({ page }) => {
-    await page.route(
-      "https://api.practicesoftwaretesting.com/products?**",
-      async (route) => {
-        const response = await page.request.fetch(route.request());
-        const responseBody = await response.json();
-        route.continue();
-      }
-    );
+    await page.route(productApiUrl, async (route) => {
+      const response = await page.request.fetch(route.request());
+      const responseBody = await response.json();
+      route.continue();
+    });
 
     const homePage = new HomePage(page);
     await homePage.goto();
 
     // This line in the code will make sure the response from the API is made before proceeding
-    await page.waitForResponse(
-      "https://api.practicesoftwaretesting.com/products?**"
-    );
+    await page.waitForResponse(productApiUrl);
   });
 });
