b5a0e495239a7153c16904458cb9b74b9ef7d8ca
Butch Mayhew <bmayhew@tilled.com>
2023-11-11 15:31:37 -0600

    updating productPage route

--- lib/fixtures/productPageRoute.ts
+++ lib/fixtures/productPageRoute.ts
@@ -22,7 +22,7 @@ export async function productIdRoute(page: any, name?: string) {
     body = await response.json();
     if (name) {
       productId = findIdByName(body, name);
-      console.log("pid: " + productId);
+      // console.log("pid: " + productId);
     } else {
       // Get the second product in the list
       productId = body.data[1].id;
@@ -31,8 +31,13 @@ export async function productIdRoute(page: any, name?: string) {
   });
 
   const homePage = new HomePage(page);
-  await homePage.goto();
-  await page.waitForResponse(productApiUrl);
+
+  // In case productId is undefined attempt to get it before we return it
+  for (let i = 0; i < 5 && !productId; i++) {
+    await homePage.goto();
+    await page.waitForResponse(productApiUrl);
+    // assuming that productId is updated here after the API call
+  }
 
   return productId;
 }
