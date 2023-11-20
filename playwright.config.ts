bce6db71c4635c52bcf76bae2aaf57c1c8dd70c2
Butch Mayhew <bmayhew@tilled.com>
2023-11-19 18:02:38 -0600

    updating to add currents.dev reporter in CI (#7)

--- playwright.config.ts
+++ playwright.config.ts
@@ -18,14 +18,16 @@ export default defineConfig<APIRequestOptions & TestOptions>({
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 2,
   workers: process.env.CI ? 1 : undefined,
-  reporter: [["html"], ["list"]],
+  reporter: process.env.CI
+    ? [["list"], ["html"], ["@currents/playwright"]]
+    : [["list"], ["html"]],
   use: {
     testIdAttribute: "data-test",
     baseURL: process.env.UI_URL,
     apiURL: process.env.API_URL,
     apiBaseURL: process.env.API_URL,
     trace: "on",
-    screenshot: "only-on-failure",
-    video: "retain-on-failure",
+    screenshot: "on",
+    video: "on",
   },
 });
